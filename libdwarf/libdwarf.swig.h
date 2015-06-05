871366ec7599e13ffa9a2e63b98357c135e5ffe6
jasper22 <jjasper22@gmail.com>
2015-06-05 12:20:01 +0300

    Added correct settings for SWIG compilation
    Removed all unused functions in libDWARF.h and libDWARF.swig.h
    Added correct .gitignore

--- libdwarf/libdwarf.swig.h
+++ libdwarf/libdwarf.swig.h
@@ -2619,6 +2619,7 @@ int dwarf_set_stringcheck(int /*stringcheck*/);
     Returns previous value.  */
 int dwarf_set_reloc_application(int /*apply*/);
 
+
 ///* Unimplemented */
 //Dwarf_Handler dwarf_seterrhand(Dwarf_Debug /*dbg*/, Dwarf_Handler /*errhand*/);
 
@@ -3337,37 +3338,37 @@ unsigned dwarf_set_harmless_error_list_size(Dwarf_Debug /*dbg*/,
 
 /* BEGIN FILE */
 
-extern int dwarf_get_TAG_name(unsigned int /*val_in*/, const char ** /*s_out */);
-extern int dwarf_get_children_name(unsigned int /*val_in*/, const char ** /*s_out */);
-extern int dwarf_get_FORM_name(unsigned int /*val_in*/, const char ** /*s_out */);
-extern int dwarf_get_AT_name(unsigned int /*val_in*/, const char ** /*s_out */);
-extern int dwarf_get_OP_name(unsigned int /*val_in*/, const char ** /*s_out */);
-extern int dwarf_get_ATE_name(unsigned int /*val_in*/, const char ** /*s_out */);
-extern int dwarf_get_DS_name(unsigned int /*val_in*/, const char ** /*s_out */);
-extern int dwarf_get_END_name(unsigned int /*val_in*/, const char ** /*s_out */);
-extern int dwarf_get_ATCF_name(unsigned int /*val_in*/, const char ** /*s_out */);
-extern int dwarf_get_ACCESS_name(unsigned int /*val_in*/, const char ** /*s_out */);
-extern int dwarf_get_VIS_name(unsigned int /*val_in*/, const char ** /*s_out */);
-extern int dwarf_get_VIRTUALITY_name(unsigned int /*val_in*/, const char ** /*s_out */);
-extern int dwarf_get_LANG_name(unsigned int /*val_in*/, const char ** /*s_out */);
-extern int dwarf_get_ID_name(unsigned int /*val_in*/, const char ** /*s_out */);
-extern int dwarf_get_CC_name(unsigned int /*val_in*/, const char ** /*s_out */);
-extern int dwarf_get_INL_name(unsigned int /*val_in*/, const char ** /*s_out */);
-extern int dwarf_get_ORD_name(unsigned int /*val_in*/, const char ** /*s_out */);
-extern int dwarf_get_DSC_name(unsigned int /*val_in*/, const char ** /*s_out */);
-extern int dwarf_get_LNS_name(unsigned int /*val_in*/, const char ** /*s_out */);
-extern int dwarf_get_LNE_name(unsigned int /*val_in*/, const char ** /*s_out */);
-extern int dwarf_get_ISA_name(unsigned int /*val_in*/, const char ** /*s_out */);
-extern int dwarf_get_MACINFO_name(unsigned int /*val_in*/, const char ** /*s_out */);
-extern int dwarf_get_CFA_name(unsigned int /*val_in*/, const char ** /*s_out */);
-extern int dwarf_get_EH_name(unsigned int /*val_in*/, const char ** /*s_out */);
-extern int dwarf_get_FRAME_name(unsigned int /*val_in*/, const char ** /*s_out */);
-extern int dwarf_get_CHILDREN_name(unsigned int /*val_in*/, const char ** /*s_out */);
-extern int dwarf_get_ADDR_name(unsigned int /*val_in*/, const char ** /*s_out */);
-extern int dwarf_get_SECT_name (unsigned int /*val_in*/,const char ** /*s_out*/);
-extern int dwarf_get_MACRO_name (unsigned int /*val_in*/,const char ** /*s_out*/);
-extern int dwarf_get_DEFAULT_name (unsigned int /*val_in*/,const char ** /*s_out*/);
-extern int dwarf_get_IDX_name (unsigned int /*val_in*/,const char ** /*s_out*/);
+//extern int dwarf_get_TAG_name(unsigned int /*val_in*/, const char ** /*s_out */);
+//extern int dwarf_get_children_name(unsigned int /*val_in*/, const char ** /*s_out */);
+//extern int dwarf_get_FORM_name(unsigned int /*val_in*/, const char ** /*s_out */);
+//extern int dwarf_get_AT_name(unsigned int /*val_in*/, const char ** /*s_out */);
+//extern int dwarf_get_OP_name(unsigned int /*val_in*/, const char ** /*s_out */);
+//extern int dwarf_get_ATE_name(unsigned int /*val_in*/, const char ** /*s_out */);
+//extern int dwarf_get_DS_name(unsigned int /*val_in*/, const char ** /*s_out */);
+//extern int dwarf_get_END_name(unsigned int /*val_in*/, const char ** /*s_out */);
+//extern int dwarf_get_ATCF_name(unsigned int /*val_in*/, const char ** /*s_out */);
+//extern int dwarf_get_ACCESS_name(unsigned int /*val_in*/, const char ** /*s_out */);
+//extern int dwarf_get_VIS_name(unsigned int /*val_in*/, const char ** /*s_out */);
+//extern int dwarf_get_VIRTUALITY_name(unsigned int /*val_in*/, const char ** /*s_out */);
+//extern int dwarf_get_LANG_name(unsigned int /*val_in*/, const char ** /*s_out */);
+//extern int dwarf_get_ID_name(unsigned int /*val_in*/, const char ** /*s_out */);
+//extern int dwarf_get_CC_name(unsigned int /*val_in*/, const char ** /*s_out */);
+//extern int dwarf_get_INL_name(unsigned int /*val_in*/, const char ** /*s_out */);
+//extern int dwarf_get_ORD_name(unsigned int /*val_in*/, const char ** /*s_out */);
+//extern int dwarf_get_DSC_name(unsigned int /*val_in*/, const char ** /*s_out */);
+//extern int dwarf_get_LNS_name(unsigned int /*val_in*/, const char ** /*s_out */);
+//extern int dwarf_get_LNE_name(unsigned int /*val_in*/, const char ** /*s_out */);
+//extern int dwarf_get_ISA_name(unsigned int /*val_in*/, const char ** /*s_out */);
+//extern int dwarf_get_MACINFO_name(unsigned int /*val_in*/, const char ** /*s_out */);
+//extern int dwarf_get_CFA_name(unsigned int /*val_in*/, const char ** /*s_out */);
+//extern int dwarf_get_EH_name(unsigned int /*val_in*/, const char ** /*s_out */);
+//extern int dwarf_get_FRAME_name(unsigned int /*val_in*/, const char ** /*s_out */);
+//extern int dwarf_get_CHILDREN_name(unsigned int /*val_in*/, const char ** /*s_out */);
+//extern int dwarf_get_ADDR_name(unsigned int /*val_in*/, const char ** /*s_out */);
+//extern int dwarf_get_SECT_name (unsigned int /*val_in*/,const char ** /*s_out*/);
+//extern int dwarf_get_MACRO_name (unsigned int /*val_in*/,const char ** /*s_out*/);
+//extern int dwarf_get_DEFAULT_name (unsigned int /*val_in*/,const char ** /*s_out*/);
+//extern int dwarf_get_IDX_name (unsigned int /*val_in*/,const char ** /*s_out*/);
 
 /* END FILE */
 
